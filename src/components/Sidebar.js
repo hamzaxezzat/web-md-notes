a90810fbbc7ad0252d03655c0604e8e3ad625061
hamzaezzzat <hamza2007ezzat@gmail.com>
2022-05-24 12:30:38 +0200

    commit

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -10,6 +10,12 @@ export default function Sidebar(props) {
                 onClick={() => props.setCurrentNoteId(note.id)}
             >
                 <h4 className="text-snippet">{note.body.split("\n")}</h4>
+                <button
+                    className="delete-btn"
+                    // Your onClick event handler here
+                >
+                    <i className="gg-trash trash-icon"></i>
+                </button>
             </div>
         </div>
     ));
